7254e2150e309ff16333129e73c78cb467d8879a
Nikk00 <43099709+Nikk00@users.noreply.github.com>
2019-06-19 18:27:37 -0400

    sitio web en laravel

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    //
+    public function home(){
+    	return view('home');
+    }
+    public function about(){
+    	return view('about');
+    }
+    public function booking(){
+    	return view('booking');
+    }
+    public function contact(){
+    	return view('contact');
+    }
+    public function packages(){
+    	return view('packages');
+    }
+    public function services(){
+    	return view('services');
+    }
+}
---
+++ resources/views/email/mensaje.blade.php
@@ -0,0 +1,5 @@
+<h1>Mensaje recibido de {{ $name }}</h1>
+<p><b> Nombre: </b>{{ $name}} </p>
+<p><b> Email: </b>{{ $email}}</p>
+<p><b> Asunto: </b>{{ $body}}</p>
+<p>{{$body}}</p>
